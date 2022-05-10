2a3909d5b52070d411743fc3dd16d8f9fcf8818c
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-05-10 22:47:35 +0100

    use images from local server

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #
 # Use bundler to start everyting
 #
-bundle exec jekyll serve --trace --config _config.yml,_config_dev.yml -d ../_site --host 0.0.0.0
+cd "`dirname \"$0\"`"
+(
+  cd ../kirstinheidler.co.uk-data
+  python3 -m http.server &
+  echo "Server started as $!"
+)
+bundle exec jekyll serve --trace --config _config.yml,_config_dev.yml,_config_local.yml -d ../_site --host 0.0.0.0
